e5451fdeb7b08f70ce38f057b2fc6aff4a241641
client <client@DESKTOP-C36L640>
2024-10-16 19:36:13 +0100

    ajouter de l'attribut age

--- src/github/HelloWorld.java
+++ src/github/HelloWorld.java
@@ -1,9 +1,15 @@
 package github;
 
 public class HelloWorld {
-
+	private  String name ;
+	private int age;
+	public void setname(String name) {
+		this.name=name;
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		
 
 	}
 
